63dc520d8fdfa3535b35cf90e4a42cf29b6b4dae
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-01-05 10:54:26 +0100

    Added sound effect when block is destroyed

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,12 @@ namespace Assets.Scripts
 {
     public class Block : MonoBehaviour
     {
+        [SerializeField] private AudioClip blockDestroySound;
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
+            AudioSource.PlayClipAtPoint(blockDestroySound, Camera.main.transform.position);
+
             Destroy(gameObject);
         }
     }
