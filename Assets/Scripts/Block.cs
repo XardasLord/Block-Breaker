3f072d484a72a5f5f9ea267e09ebd27dcc0892d2
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-01-05 11:24:56 +0100

    Loading next level mechanism

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,13 +4,28 @@ namespace Assets.Scripts
 {
     public class Block : MonoBehaviour
     {
-        [SerializeField] private AudioClip blockDestroySound;
+        [SerializeField] private AudioClip _blockDestroySound;
+
+        private Level _level;
+
+        private void Start()
+        {
+            _level = FindObjectOfType<Level>();
+            _level.CountBreakableBlocks();
+        }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            AudioSource.PlayClipAtPoint(blockDestroySound, Camera.main.transform.position);
+            DestroyBlock();
+        }
+
+        private void DestroyBlock()
+        {
+            AudioSource.PlayClipAtPoint(_blockDestroySound, Camera.main.transform.position);
 
             Destroy(gameObject);
+
+            _level.BlockDestroyed();
         }
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Level : MonoBehaviour
+    {
+        private int _breakableBlocks;
+        private SceneLoader _sceneLoader;
+
+        private void Start()
+        {
+            _sceneLoader = FindObjectOfType<SceneLoader>();
+        }
+
+        public void CountBreakableBlocks()
+        {
+            _breakableBlocks++;
+        }
+
+        public void BlockDestroyed()
+        {
+            _breakableBlocks--;
+
+            if (_breakableBlocks <= 0)
+            {
+                _sceneLoader.LoadNextScene();
+            }
+        }
+    }
+}
+
